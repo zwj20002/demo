02c75c8034928a4388dffa20d7e292a45c44c0d5
zwj <3086526860@qq.com>
2025-04-08 12:17:33 +0800

    ls

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,70 +1,69 @@
 // router - intex.ts
-import { storeToRefs } from 'pinia'
-import { loginStore } from '@/store/login'
+import { storeToRefs } from "pinia";
+import { loginStore } from "@/store/login";
 // append import
 // import newsRoutes from './modules/newsRouter'
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: () => import('@/views/home/index.vue')
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/home/index.vue"),
   },
   {
-    path: '/privilege',
-    name: 'Privilege',
-    component: () => import('@/views/privilege/index.vue')
+    path: "/privilege",
+    name: "Privilege",
+    component: () => import("@/views/privilege/index.vue"),
   },
   {
-    path: '/my',
-    name: 'My',
-    component: () => import('@/views/my/index.vue')
+    path: "/my",
+    name: "My",
+    component: () => import("@/views/my/index.vue"),
   },
   {
-    path: '/withdrawal',
-    name: 'Withdrawal',
-    component: () => import('@/views/my/withdrawal.vue')
+    path: "/withdrawal",
+    name: "Withdrawal",
+    component: () => import("@/views/my/withdrawal.vue"),
   },
   {
-  path: '/seeting',
-  name: 'Seeting',
-  component: () => import('@/views/my/seeting.vue')
-},
-{
-  path: '/advice',
-  name: 'Advice',
-  component: () => import('@/views/my/advice.vue')
-},
-{
-  path: '/money',
-  name: 'Money',
-  component: () => import('@/views/my/money.vue')
-},
+    path: "/seeting",
+    name: "Seeting",
+    component: () => import("@/views/my/seeting.vue"),
+  },
+  {
+    path: "/advice",
+    name: "Advice",
+    component: () => import("@/views/my/advice.vue"),
+  },
+  {
+    path: "/money",
+    name: "Money",
+    component: () => import("@/views/my/money.vue"),
+  },
 
   {
-    path: '/login',
-    name: 'Login',
-    component: () =>
-      import('@/views/login/login.vue'),
-    meta: { title: '登录', showNavbar: false },
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/login/login.vue"),
+    meta: { title: "登录", showNavbar: false },
     beforeEnter: (_to: any, _from: any, next: any) => {
-      const store = loginStore()
-      const { token } = storeToRefs(store)
-      token.value ? next('/') : next()
-    }
+      const store = loginStore();
+      const { token } = storeToRefs(store);
+      token.value ? next("/") : next();
+    },
   },
   // ...newsRoutes,
   // append new router
-]
+];
 
 const router = createRouter({
   routes,
-  history: createWebHashHistory()
-})
+  history: createWebHashHistory(),
+});
 
-export default router
+export default router;
